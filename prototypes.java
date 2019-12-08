d4feb11079fc1405cff6c789701a5a0f9a09a481
Max Loeffler <max_loeffler@eogresources.com>
2019-12-08 21:38:36 +0000

    class header syntax

--- prototypes.java
+++ prototypes.java
@@ -1,31 +1,36 @@
-SystemDB:
+public class SystemDB {
     public void updateStatistics(Order order) {
         // update stats for the given order
     }
+}
 
 
-markCompleteHandler:
+public class markCompleteHandler {
     public void complete(Order order) {
         activeOrderQueue.popOrder(Order)
         completeOrderQueue.pushOrder(Order) 
     }
+ }
 
 
-recordItemHandler:
+public class recordItemHandler {
     public void addItem(string name, int qty) {
         oi = OrderItem(name, qty)
         order.pushItem(oi)
     }
+}
 
 
-ProcessHandler:
+public class ProcessHandler {
     public void process(Order order){
         takenOrderQueue.popOrder(order);
         activeOrderQueue.pushOrder(order);
     }
+}
 
 
-completeOrderHandler:
+public class completeOrderHandler {
     public void record(Order order) {
         takenOrderQueue.pushOrder(order);
     }
+}
