57d66b23bb5079e3dc7a3842528acda84700fec5
Max Loeffler <max_loeffler@eogresources.com>
2019-12-08 21:44:46 +0000

    updated included objects to match use case

--- prototypes.java
+++ prototypes.java
@@ -1,10 +1,3 @@
-public class SystemDB {
-    public void updateStatistics(Order order) {
-        // update stats for the given order
-    }
-}
-
-
 public class markCompleteHandler {
     public void complete(Order order) {
         activeOrderQueue.popOrder(Order)
@@ -14,17 +7,9 @@ public class markCompleteHandler {
 
 
 public class recordItemHandler {
-    public void addItem(string name, int qty) {
+    public void addItem(String name, int qty) {
         oi = OrderItem(name, qty)
         order.pushItem(oi)
-    }
-}
-
-
-public class ProcessHandler {
-    public void process(Order order){
-        takenOrderQueue.popOrder(order);
-        activeOrderQueue.pushOrder(order);
     }
 }
 
@@ -32,5 +17,27 @@ public class ProcessHandler {
 public class completeOrderHandler {
     public void record(Order order) {
         takenOrderQueue.pushOrder(order);
+    }
+}
+
+
+public class TakenOrderQueue {
+    public void pushOrder(Order order){
+        list.add(order);
+    }
+ 
+    public void popOrder(Order order){
+        return list.remove(order);
+    }
+}
+
+
+public class OrderItem {
+}
+
+
+public class Order {
+    public void pushItem(OrderItem item){
+        list.add(item);
     }
 }
